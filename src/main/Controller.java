d660da2462f7285af9b39e37e686a0cacc849349
artzai <frio12@live.com>
2024-03-29 12:45:10 -0600

    Cleans some code and adds function where the button is disabled if input is not valid.

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -15,6 +15,7 @@ import javax.swing.event.CaretEvent;
 
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -25,6 +26,7 @@ import enums.CurrencyUnit;
 import enums.TemperatureUnit;
 import util.CurrencyConverter;
 import util.TemperatureConverter;
+import util.ValidateInput;
 import net.miginfocom.swing.MigLayout;
 
 @SuppressWarnings("serial")
@@ -35,12 +37,31 @@ public class Controller extends JFrame{
 	private JTextField outputTextField;
 	
 	/**
+	 * Launch the application
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Controller frame = new Controller();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	/**
 	 * Create the frame.
 	 */
 	public Controller() {
-//		Atributes
+//		Attributes
 		Font font = new Font("Montserrat", Font.PLAIN, 14);
 		
+		/**
+		 * Window and elements setup
+		 */
 //		ContentPane
 		setTitle("Unit Conversor");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -49,24 +70,55 @@ public class Controller extends JFrame{
 		contentPane.setToolTipText("");
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
 		contentPane.setLayout(new MigLayout("", "[grow][grow]", "[20px][][][][25px:n][][][][grow]"));
+		setContentPane(contentPane);
 		
 //		Label
 		JLabel titleLabel = new JLabel("Choose the type of units to convert");
 		titleLabel.setFont(font);
 		contentPane.add(titleLabel, "cell 0 0 3 1");
 		
-//		SelectorPane
+//		TypeSelector
+		JRadioButton currenciesRadioButton = new JRadioButton("Currencies");
+		currenciesRadioButton.setHorizontalAlignment(SwingConstants.LEFT);
+		currenciesRadioButton.setSelected(true);
+		contentPane.add(currenciesRadioButton, "cell 1 1");
+		
+		JRadioButton temperaturesRadioButton = new JRadioButton("Temperatures");
+		temperaturesRadioButton.setHorizontalAlignment(SwingConstants.RIGHT);
+		temperaturesRadioButton.setFont(font);
+		contentPane.add(temperaturesRadioButton, "cell 1 1");
+		
+		ButtonGroup units = new ButtonGroup();
+		units.add(currenciesRadioButton);
+		units.add(temperaturesRadioButton);
+		
+//		TextField
+		inputTextField = new JTextField();
+		inputTextField.setFont(font);
+		contentPane.add(inputTextField, "cell 0 3,growx");
+		
+		JLabel validationErrorLabel = new JLabel();
+		validationErrorLabel.setFont(font);
+		validationErrorLabel.setForeground(new Color(255, 0, 0));
+		validationErrorLabel.setVisible(false);
+		contentPane.add(validationErrorLabel, "cell 0 4 2 1");
+
+		outputTextField = new JTextField();
+		outputTextField.setFont(font);
+		outputTextField.setEditable(false);
+		contentPane.add(outputTextField, "cell 0 5,growx");
+		
+//		UnitSelectorPane
 		JPanel subSelectorTopPane = new JPanel();
-		contentPane.add(subSelectorTopPane, "cell 1 3,grow");
 		subSelectorTopPane.setLayout(new CardLayout(0, 0));
+		contentPane.add(subSelectorTopPane, "cell 1 3,grow");
 
 		JPanel subSelectorBottomPane = new JPanel();
-		contentPane.add(subSelectorBottomPane, "cell 1 5,grow");
 		subSelectorBottomPane.setLayout(new CardLayout(0, 0));
+		contentPane.add(subSelectorBottomPane, "cell 1 5,grow");
 
-//		SubSelector
+//		UnitSelectors
 		JComboBox<CurrencyUnit> currenciesTopBox = new JComboBox<>(CurrencyUnit.values());
 		currenciesTopBox.setSelectedIndex(0);
 		subSelectorTopPane.add(currenciesTopBox);
@@ -85,9 +137,45 @@ public class Controller extends JFrame{
 		subSelectorBottomPane.add(temperaturesBottomBox);
 		temperaturesBottomBox.setVisible(false);
 		
+//		ConvertButton
+		JButton convertButton = new JButton("Convert");
+		contentPane.add(convertButton, "cell 1 7");
+		
+//		Action listeners
 		/**
+		 * currenciesRadioButton
+		 * temperaturesRadioButton
+		 * 
+		 * This action listeners change the value of witch boxes are visible
+		 * depending on the radio button selected
+		 */
+		currenciesRadioButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				temperaturesTopBox.setVisible(false);
+				temperaturesBottomBox.setVisible(false);
+				currenciesTopBox.setVisible(true);
+				currenciesBottomBox.setVisible(true);
+			}
+		});
+		
+		temperaturesRadioButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				temperaturesTopBox.setVisible(true);
+				temperaturesBottomBox.setVisible(true);
+				currenciesTopBox.setVisible(false);
+				currenciesBottomBox.setVisible(false);
+			}
+		});
+		
+		/**
+		 * temperaturesTopBox
+		 * temperaturesBottomBox
+		 * currenciesTopBox
+		 * currenciesBottomBox
+		 * 
 		 * This action listeners change the value of the non-selected box
 		 * to un-match the value of the selected one
+		 * i.e: There can't be a conversion from Celsius to Celsius
 		 */
 		temperaturesTopBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -137,68 +225,27 @@ public class Controller extends JFrame{
 			}
 		});
 
-		// Text fields
-		JLabel validationErrorLabel = new JLabel();
-		validationErrorLabel.setFont(font);
-		validationErrorLabel.setForeground(new Color(255, 0, 0));
-		validationErrorLabel.setVisible(false);
-		contentPane.add(validationErrorLabel, "cell 0 4 2 1");
-		
-		inputTextField = new JTextField();
-		inputTextField.setFont(font);
-		contentPane.add(inputTextField, "cell 0 3,growx");
+		/**
+		 * This action listener enables input validation in real time
+		 */
 		inputTextField.addCaretListener(new CaretListener() {
 			public void caretUpdate(CaretEvent e) {
 				if (!ValidateInput.validateInput(inputTextField.getText().toString())) {
 					inputTextField.setForeground(new Color(255, 0, 0));
 					validationErrorLabel.setText(ValidateInput.getValidationError());
 					validationErrorLabel.setVisible(true);
+					convertButton.setEnabled(false);
 				} else {
 					inputTextField.setForeground(new Color(0, 0, 0));
 					validationErrorLabel.setVisible(false);
+					convertButton.setEnabled(true);
 				}
 			}
 		});
 		
-		outputTextField = new JTextField();
-		outputTextField.setFont(font);
-		outputTextField.setEditable(false);
-		contentPane.add(outputTextField, "cell 0 5,growx");
-
-//		TypeSelector
-		JRadioButton currenciesRadioButton = new JRadioButton("Currencies");
-		currenciesRadioButton.setHorizontalAlignment(SwingConstants.LEFT);
-		currenciesRadioButton.setFont(font);
-		currenciesRadioButton.setSelected(true);
-		currenciesRadioButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				temperaturesTopBox.setVisible(false);
-				temperaturesBottomBox.setVisible(false);
-				currenciesTopBox.setVisible(true);
-				currenciesBottomBox.setVisible(true);
-			}
-		});
-		contentPane.add(currenciesRadioButton, "cell 0 1");
-
-		JRadioButton temperaturesRadioButton = new JRadioButton("Temperatures");
-		temperaturesRadioButton.setHorizontalAlignment(SwingConstants.RIGHT);
-		temperaturesRadioButton.setFont(font);
-		temperaturesRadioButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				temperaturesTopBox.setVisible(true);
-				temperaturesBottomBox.setVisible(true);
-				currenciesTopBox.setVisible(false);
-				currenciesBottomBox.setVisible(false);
-			}
-		});
-		contentPane.add(temperaturesRadioButton, "cell 1 1");
-
-		ButtonGroup units = new ButtonGroup();
-		units.add(currenciesRadioButton);
-		units.add(temperaturesRadioButton);
-		
-//		ConvertButton
-		JButton convertButton = new JButton("Convert");
+		/**
+		 * This action listener perfoms the conversion and return the output value
+		 */
 		convertButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -219,7 +266,7 @@ public class Controller extends JFrame{
 				}
 			}
 		});
-		contentPane.add(convertButton, "cell 1 7");
+		
 		
 	}
 
